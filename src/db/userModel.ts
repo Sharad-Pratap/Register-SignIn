f465372ac3107fcda9f98747f58ca998a25984f9
Sharad-Pratap <sharadpratap40@gmail.com>
2024-01-20 16:55:15 +0530

    first commit

---
+++ src/db/userModel.ts
@@ -0,0 +1,18 @@
+// src/models/User.ts
+import mongoose, { Document } from 'mongoose';
+
+export interface IUser extends Document {
+  username: string;
+  password: string;
+  email: string;
+//   role: 'student' | 'examiner'; //use if working on role based model
+}
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email : {type: String, required : true},
+//   role: { type: String, enum: ['student', 'examiner'], required: true },  //use if working on role based model
+});
+
+export default mongoose.model<IUser>('User', userSchema);
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import authRoute from './routes/authRoute';
+
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.Router());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
+
+
+
+app.use("/api", authRoute);
+
+
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.DATABASEURL as string).then(() => {
+    console.log("database connection established");
+}).catch((error) => {
+    console.log(error);
+});
+
+
+app.listen(3001, ()=>{
+    console.log("server running on port 3001");
+})
